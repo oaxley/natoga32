2d87fdffc28eb29ae80e4a59e3ec5169a2a5f249
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-07 21:39:07 -0500

    Perform basic checks when loading the configuration

--- compiler/packages/config.py
+++ compiler/packages/config.py
@@ -15,6 +15,8 @@
 from __future__ import annotations
 from typing import Any, Dict, List
 
+import os
+
 from argparse import Namespace
 from dataclasses import dataclass
 
@@ -33,10 +35,18 @@ class Config():
         Args:
             args: the Namespace from the command line parser
         """
-        # input / output file
-        self.input_file = args.file
+        # input file checks
+        if self.is_exist(args.file):
+            self.input_file = args.file
+        else:
+            raise FileNotFoundError("Error: Unable to find the input file!")
+
+        # output file checks
         if args.output is None:
             self.output_file = "a.out"
         else:
             self.output_file = args.output
 
+    def is_exist(self, filename: str) -> bool:
+        """check if the file exists"""
+        return os.path.exists(filename)
