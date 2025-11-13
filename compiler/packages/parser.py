d756b8fd0ffb577275f3fed79fa656bd832a2121
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-12 21:30:22 -0500

    Build Parser internals

--- compiler/packages/parser.py
+++ compiler/packages/parser.py
@@ -13,9 +13,12 @@
 
 #----- imports
 from __future__ import annotations
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
+
+import packages.ast as ast
 
 from packages.token import TokenStream
+from packages.specs import TokenType
 
 #----- class
 class Parser:
@@ -25,6 +28,48 @@ class Parser:
         """Constructor"""
         self.tokens = tokens
 
-    def parse(self) -> None:
-        while (token := self.tokens.next()):
-            print(token)
+    def process(self) -> ast.Program:
+        """Process the tokens and create the AST"""
+        statements: List[ast.Statement] = []
+        while (not self.tokens.end()):
+            stmt = self.parse()
+            if stmt:
+                statements.append(stmt)
+
+        return ast.Program(statements)
+
+    def parse(self) -> Optional[ast.Statement]:
+        """Process tokens and build a stateemt"""
+        # retrieve the next token
+        token = self.tokens.peek()
+        if token is None:
+            return None
+
+        # check for directives
+        if token.type == TokenType.DIRECTIVE:
+            return self._parse_directive()
+
+        # unknown token
+        self.tokens.next()
+        return None
+
+    def _parse_directive(self) -> Optional[ast.Directive]:
+        """Parse the tokens and build a directive"""
+        # retrieve the next token
+        token = self.tokens.next()
+        if not token:
+            return None
+
+        # ensure a directive starts with an '.' at the begining
+        if token.type == TokenType.DIRECTIVE and token.value[0] != '.':
+            raise SyntaxError(f"Error ({token.row},{token.col}): '{token.value}' recognized as DIRECTIVE without starting with '.'")
+
+        # retrieve the args for this directive
+        args = []
+        t = self.tokens.next()
+        while (t and t.type != TokenType.EOL):
+            args.append(t.value)
+            t = self.tokens.next()
+
+        return ast.Directive(token.value, args)
+
