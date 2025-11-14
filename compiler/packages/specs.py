56e52b8936e5c9d33c3fe887ef17cc0f15dec2f6
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-13 23:24:05 -0500

    Add Function regexp for macro definition

--- compiler/packages/specs.py
+++ compiler/packages/specs.py
@@ -31,6 +31,7 @@ class TokenType(IntEnum):
     IDENT = auto()                      # identifier
     LABEL = auto()                      # any identifier terminated with ':'
     MACRO = auto()                      # assembler macro
+    FUNCTION = auto()                   # macro function
     STRING = auto()                     # a "string"
     CHAR = auto()                       # a single char 'A'
 
@@ -62,6 +63,7 @@ class TokenType(IntEnum):
 TOKENS_SPECS = [
     (TokenType.NUMBER.name, r'0x[a-zA-Z0-9_]+|0b[01_]+|\b\d+\b'),
     (TokenType.DIRECTIVE.name, r'\.[A-Za-z_][A-Za-z0-9_]*'),
+    (TokenType.FUNCTION.name, r'[A-Za-z_][A-Za-z0-9_]*\('),
     (TokenType.LABEL.name, r'[A-Za-z_][A-Za-z0-9_]*:'),
     (TokenType.IDENT.name, r'[A-Za-z_][A-Za-z0-9_-]*'),
     (TokenType.MACRO.name, r'\%[A-Za-z_]+'),
