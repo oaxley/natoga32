9dc463cfe3d46d5241cdb2316b66101eb33dfcb0
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-17 23:06:43 -0500

    Complete rewrite of the parser

--- compiler/packages/parser.py
+++ compiler/packages/parser.py
@@ -13,12 +13,13 @@
 
 #----- imports
 from __future__ import annotations
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import packages.ast as ast
 
-from packages.token import TokenStream, Token
+from packages.token import TokenStream
 from packages.specs import TokenType
+
 
 #----- class
 class Parser:
@@ -26,45 +27,180 @@ class Parser:
 
     def __init__(self, tokens: TokenStream) -> None:
         """Constructor"""
-        self.tokens = tokens
+        self.ts = tokens
 
-    def process(self) -> ast.Program:
-        """Process the tokens and create the AST"""
-        statements: List[ast.Statement] = []
-        while (not self.tokens.end()):
-            stmt = self.parse()
-            if stmt:
-                statements.append(stmt)
+    def parse_program(self) -> ast.Program:
+        stmts: List[ast.Statement] = []
 
-        return ast.Program(statements)
+        while not self.ts.at_end():
+            # skip possible standalone EOL
+            if self.ts.expect(TokenType.EOL):
+                self.ts.advance()
+                continue
 
-    def parse(self):
-        # retrieve all the tokens for this statement
-        tokens: List[Token] = []
-        while True:
-            t = self.tokens.next()
-            if not t or t.type in [TokenType.EOL, TokenType.EOF]:
+            # end parsing on EOF
+            if self.ts.expect(TokenType.EOF):
                 break
-            else:
-                tokens.append(t)
 
-        # print(f"{' '.join(map(str, tokens))} {len(tokens)}")
-        if len(tokens) == 0:
+            stmt = self.parse_statement()
+            if stmt:
+                stmts.append(stmt)
+
+        return ast.Program(stmts)
+
+    def parse_statement(self) -> Optional[ast.Statement]:
+        token = self.ts.peek()
+        if not token:
             return None
 
-        # single directive or label
-        if len(tokens) == 1:
-            t = tokens[0]
-            if t.type == TokenType.LABEL:
-                return ast.Label(t.value[:-1])
-            elif t.type == TokenType.DIRECTIVE:
-                return ast.Directive(t.value, None, None)
-            elif t.type == TokenType.IDENT:
-                return ast.Instruction(t.value, [])
+        if token.type == TokenType.EOL:
+            self.ts.advance()
+            return None
+
+        if token.type == TokenType.LABEL:
+            return self.parse_label()
+
+        if token.type in [TokenType.DIRECTIVE, TokenType.IDENT]:
+            # different cases:
+            # IDENT followed by DIRECTIVE -> VALUE .equ ...
+            # DIRECTIVE alone -> .data, .text
+            # IDENT then sth else -> instruction
+            if token.type == TokenType.IDENT:
+                next_token = self.ts.peek(1)
+                if next_token and next_token.type == TokenType.DIRECTIVE:
+                    return self.parse_directive()
+                else:
+                    return self.parse_instruction()
             else:
-                raise SyntaxError(f"Unknown token [{t.value}] at ({t.row}, {t.col})")
+                return self.parse_directive()
 
-        return None
+        raise SyntaxError(f"Unexpected token: {token.type.name} ({token.row}, {token.col})")
 
+    def parse_label(self) -> ast.Label:
+        token = self.ts.advance()
+        name = token.value              # type: ignore
 
+        # remove the trailing ':' if any
+        if name.endswith(':'):
+            name = name[:-1]
+
+        if self.ts.expect(TokenType.EOL):
+            self.ts.advance()
+
+        return ast.Label(name)
+
+    def parse_directive(self) -> ast.Directive:
+        # possible patterns:
+        # DIRECTIVE ...
+        # IDENT DIRECTIVE ...
+        label_name: Optional[str] = None
+        next_token = self.ts.peek(1)
+        if self.ts.expect(TokenType.IDENT) and next_token and next_token.type == TokenType.DIRECTIVE:
+            # form: IDENT DIRECTIVE ...
+            label_name = self.ts.advance().value    # type: ignore
+
+        dir_tok = self.ts.advance()
+        if not dir_tok or dir_tok.type != TokenType.DIRECTIVE:
+            raise SyntaxError(f"Expected DIRECTIVE token!")
+
+        args: List[ast.Node] = []
+        while True:
+            token = self.ts.peek()
+
+            if not token or token.type in [TokenType.EOL, TokenType.EOF]:
+                break
+
+            # remove COMMA
+            if token.type == TokenType.COMMA:
+                self.ts.advance()
+                continue
+
+            # parse possible token types: number, string, char, ident, comma ...
+            if token.type == TokenType.NUMBER:
+                t = self.ts.advance()
+                args.append(ast.Number(int(t.value, 0))) # type: ignore
+
+            elif token.type == TokenType.STRING:
+                t = self.ts.advance()
+                # strip the quotes around the string
+                s = t.value    # type: ignore
+                if len(s) >= 2 and s[0] == '"' and s[-1] == '"':
+                    s = s[1:-1]
+                args.append(ast.StringLiteral(s))
+
+            elif token.type == TokenType.CHAR:
+                t = self.ts.advance()
+                # strip simple quotes
+                s = t.value    # type: ignore
+                if len(s) >= 2 and s[0] == "'" and s[-1] == "'":
+                    s = s[1:-1]
+                args.append(ast.CharLiteral(s))
+
+            elif token.type == TokenType.IDENT:
+                t = self.ts.advance()
+                args.append(ast.Identifier(t.value)) # type: ignore
+
+            else:
+                # unknown token. Add it to the list as Identifier to consume it
+                t = self.ts.advance()
+                args.append(ast.Identifier(t.value)) # type: ignore
+
+        # consume the trailing EOL if there
+        if self.ts.expect(TokenType.EOL):
+            self.ts.advance()
+
+        return ast.Directive(dir_tok.value, args, label_name)
+
+    def parse_instruction(self) -> ast.Instruction:
+        # instruction: IDENT <operands separated by comma>
+
+        op_tok = self.ts.advance()
+        if not op_tok or op_tok.type != TokenType.IDENT:
+            raise SyntaxError(f"Expected instruction opcode (IDENT)")
+
+        args: List[Union[ast.Node,str,int]] = []
+        while True:
+            token = self.ts.peek()
+            if not token or token.type in [TokenType.EOL, TokenType.EOF]:
+                break
+
+            # remove COMMA
+            if token.type == TokenType.COMMA:
+                self.ts.advance()
+                continue
+
+            # parse possible token types: number, string, char, ident, comma ...
+            if token.type == TokenType.IDENT:
+                t = self.ts.advance()
+                args.append(ast.Identifier(t.value)) # type: ignore
+
+            elif token.type == TokenType.NUMBER:
+                t = self.ts.advance()
+                args.append(ast.Number(int(t.value, 0))) # type: ignore
+
+            elif token.type == TokenType.STRING:
+                t = self.ts.advance()
+                # strip the quotes around the string
+                s = t.value    # type: ignore
+                if len(s) >= 2 and s[0] == '"' and s[-1] == '"':
+                    s = s[1:-1]
+                args.append(ast.StringLiteral(s))
+
+            elif token.type == TokenType.CHAR:
+                t = self.ts.advance()
+                # strip simple quotes
+                s = t.value    # type: ignore
+                if len(s) >= 2 and s[0] == "'" and s[-1] == "'":
+                    s = s[1:-1]
+                args.append(ast.CharLiteral(s))
+
+            else:
+                # unknown token. Add it to the list as Identifier to consume it
+                t = self.ts.advance()
+                args.append(ast.Identifier(t.value)) # type: ignore
+
+        if self.ts.expect(TokenType.EOL):
+            self.ts.advance()
+
+        return ast.Instruction(op_tok.value, args)
 
