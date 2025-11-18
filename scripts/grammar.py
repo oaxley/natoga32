d48752c15c5b1c3ffe7da1ec189be4b515e8cd77
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-17 23:01:58 -0500

    Add statistics and colors to highlight results

--- scripts/grammar.py
+++ scripts/grammar.py
@@ -17,18 +17,24 @@ args = parser.parse_args()
 with open(args.ebnf, "r") as fh:
     content = fh.read()
 
-parser = Lark(content, parser="earley", start="start", debug=True)
+parser = Lark(content, parser="earley", start="start", debug=False)
 
 
 # open the test file
 if args.test:
+    lines = 0
+    count = 0
     with open(args.test, "r") as fh:
         for line in fh:
-            line = line.rstrip("\n\r")
+            line = line.rstrip("\n\r").lstrip(" ")
             if len(line) > 0:
+                lines += 1
                 try:
-                    print(f"--- {line}")
                     tree = parser.parse(line)
+                    print(f"\033[90m--- {line}\033[0m")
                     print(tree.pretty())
+                    count += 1
                 except (Except.UnexpectedCharacters, Except.UnexpectedEOF):
-                    pass
+                    print(f"\033[91m--- {line}\033[0m")
+
+    print(f"Coverage: {count} out of {lines} ({100.0 *count/lines:0.02f} %)")
