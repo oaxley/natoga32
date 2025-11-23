8f2a65b6fd85458bec90aac6467fdf19e77187ea
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-23 16:09:27 -0500

    Cosmetic change

--- compiler/packages/preprocessor/helper.py
+++ compiler/packages/preprocessor/helper.py
@@ -15,9 +15,7 @@
 from __future__ import annotations
 from typing import Any, Dict, List, Optional
 
-from packages.token import (
-    Token, TokenStream, TokenType
-)
+from packages.token import Token, TokenStream, TokenType
 
 
 #----- globals
