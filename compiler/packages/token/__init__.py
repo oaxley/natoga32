5eba6c5c3521fd1ee072c523794784112f43c292
slegrand <ut5svsxgc@relay.firefox.com>
2025-11-23 15:37:40 -0500

    Split token files and move to its own module directory

---
+++ compiler/packages/token/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the MIT License
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/license/mit
+#
+# @author	Sebastien LEGRAND
+# @license	MIT License
+#
+# @brief	__init__ file for token package
+
+#----- imports
+from token_dataclass import Token
+from token_stream import TokenStream
+from token_type import TokenType
---
+++ compiler/packages/token/token_type.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the MIT License
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/license/mit
+#
+# @author	Sebastien LEGRAND
+# @license	MIT License
+#
+# @brief	TokenType definition
+
+#----- imports
+from __future__ import annotations
+from typing import Any, Dict, List
+
+from enum import IntEnum, auto
+
+
+#----- classes
+
+class TokenType(IntEnum):
+    # control characters
+    EOF = auto()                        # end of file
+    EOL = auto()                        # end of line '\n'
+
+    # numbers & directives
+    NUMBER = auto()                     # any numbers
+    DIRECTIVE = auto()                  # assembler directive
+    IDENT = auto()                      # identifier
+    LABEL = auto()                      # any identifier terminated with ':'
+    STRING = auto()                     # a "string"
+    CHAR = auto()                       # a single char 'A'
+    ENVVAR = auto()
+
+    # operators & symbols
+    EQUAL = auto()                      # '='
+    LPARENT = auto()                    # '('
+    RPARENT = auto()                    # ')'
+    COMMA = auto()                      # ','
+    PLUS = auto()                       # '+'
+    MINUS = auto()                      # '-'
+    STAR = auto()                       # '*'
+    SLASH = auto()                      # '/'
+    MODULO = auto()                     # '%'
+    LSHIFT = auto()                     # '<<'
+    RSHIFT = auto()                     # '>>'
+    AND = auto()                        # '&'
+    OR = auto()                         # '|'
+    XOR = auto()                        # '^'
+    DOLLAR = auto()                     # alias for current PC
+
+    # misc
+    PASTE = auto()                      # token pasting '##'
+    SKIP = auto()                       # spaces and tabs
+    COMMENT = auto()
+
+    # unknown type
+    UNKNOWN = auto()
