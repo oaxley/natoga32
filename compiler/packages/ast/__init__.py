649f95d4a3c7567268e1a056135c30695c0ee016
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-14 18:13:25 -0500

    Add all imports to ast package

---
+++ compiler/packages/ast/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the MIT License
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/license/mit
+#
+# @author	Sebastien LEGRAND
+# @license	MIT License
+#
+# @brief	Abstract Syntax Tree package initializer
+
+#----- imports
+from .node import Node, Statement, Program, Expression
+from .statement import Directive, Instruction, Label
+from .expression import (
+    Identifier, Number, StringLiteral, CharLiteral,
+    BinaryOp, UnaryOp, CurrentPC,
+    HiRel, LoRel, PCRelHi, PCRelLo
+)
