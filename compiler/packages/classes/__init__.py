a9ac0c6b37c39c2cf7838c72ebf56abdd9325006
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-14 22:21:40 -0500

    Add classes package initializer

---
+++ compiler/packages/classes/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the MIT License
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/license/mit
+#
+# @author	Sebastien LEGRAND
+# @license	MIT License
+#
+# @brief	Classes package initializer
+
+#----- imports
+from .symbol_table import SymbolTable
+from .token_stream import TokenStream
