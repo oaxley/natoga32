837701bf8eb8d9aa4f3613c0156308134bdd2540
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-14 23:43:14 -0500

    Restart compiler source from scratch

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -20,128 +20,158 @@ import sys
 
 from argparse import ArgumentParser
 
-import packages.ast as ast
-
-from packages.config import Config
+from packages.structs import Config
 from packages.lexer import Lexer
-from packages.parser import Parser
-from packages.preprocessor import PreProcessor
-from packages.symbols import SymbolTable
-from packages.semantic import SemanticAnalyzer
-from packages.data_classes import Section
-from packages.pseudo_instr import PseudoInstruction
+from packages.pre_processor import PreProcessor
+
+# from packages.parser import Parser
+# from packages.preprocessor import PreProcessor
+# from packages.symbols import SymbolTable
+# from packages.semantic import SemanticAnalyzer
+# from packages.data_classes import Section
+# from packages.pseudo_instr import PseudoInstruction
 
 
 # ----- begin
 
 # parse the command line arguments
 argparse = ArgumentParser()
-argparse.add_argument("file", help="assembler file to compile")
-argparse.add_argument("-o", "--output", help="output file")
+argparse.add_argument("infile", help="assembler file to compile")
+argparse.add_argument("-o", "--outfile", help="output file")
 argparse.add_argument("-d", "--debug", type=int, help="output file")
 args = argparse.parse_args()
 
 # initialize the configuration
 try:
     config = Config(args)
+    print(f"Compiling '{config.infile}' to '{config.outfile}' ..." )
 except FileNotFoundError as e:
     print(e)
     sys.exit(1)
 
-# open the file
-fh = open(config.input_file, "r", encoding="utf-8")
-
 try:
-    # global symbol table
-    symbols = SymbolTable()
-
-    # create a new lexer
+    # Step 1: Lexer
     step = 1
     print(f"Phase {step} : Lexer")
     lexer = Lexer()
-    lexer.parse(fh)
+    lexer.parse(config)
+    config.tokens = lexer.tokens
 
     if args.debug == step:
         for i in lexer.tokens:
             print(i)
         sys.exit(0)
 
-    # pre-processor
+    # Step 2: Pre-processor
     step += 1
     print(f"Phase {step} : Pre-processor")
-    preproc = PreProcessor(symbols)
-    tokens = preproc.process(lexer.tokens, config.input_file)
+    preproc = PreProcessor(config)
+    out = preproc.process()
 
     if args.debug == step:
-        for i in tokens:
+        for i in out:
             print(i)
-        sys.exit(0)
-
-    # parser
-    step += 1
-    print(f"Phase {step} : Parser")
-    parser = Parser(tokens)
-    program = parser.parse_program()
-
-    if args.debug == step:
-        program.debug()
-        sys.exit(0)
-
-
-    # pseudo instruction expansion
-    step += 1
-    pseudo = PseudoInstruction(program)
-    program = pseudo.process()
-
-    if args.debug == step:
-        program.debug()
-        sys.exit(0)
-
-
-    # define the assembly sections
-    sections: Dict[str, Section] = {
-        ".text": Section(".text"),
-        ".data": Section(".data"),
-        ".bss": Section(".bss")
-    }
-
-    # semantic analyzer
-    step += 1
-    print(f"Phase {step} : Semantic Analyzer - First Pass")
-    semantic = SemanticAnalyzer(program, symbols, sections)
-    semantic.first_pass()
-
-    if args.debug == step:
-        print("==== SECTIONS ====")
-        for v in sections.items():
-            print(v[0])
-            print(v[1])
-            print()
-
-        print("\n==== SYMBOLS ====")
-        for k, v in symbols.items:
-            print(v)
 
         sys.exit(0)
-
-    # semantic phase 2
-    step += 1
-    print(f"Phase {step} : Semantic Analyzer - Second Pass")
-    semantic.second_pass()
-
-    if args.debug == step:
-        print("==== SECTIONS ====")
-        for v in sections.items():
-            print(v[0])
-            print(v[1])
-            print()
-
-        print("\n==== SYMBOLS ====")
-        for k, v in symbols.items:
-            print(v)
-
-        sys.exit(0)
-
 
 except SyntaxError as e:
     print(str(e))
+
+
+# # open the file
+# fh = open(config.input_file, "r", encoding="utf-8")
+
+# try:
+#     # global symbol table
+#     symbols = SymbolTable()
+
+#     # create a new lexer
+#     step = 1
+#     print(f"Phase {step} : Lexer")
+#     lexer = Lexer()
+#     lexer.parse(fh)
+
+#     if args.debug == step:
+#         for i in lexer.tokens:
+#             print(i)
+#         sys.exit(0)
+
+#     # pre-processor
+#     step += 1
+#     print(f"Phase {step} : Pre-processor")
+#     preproc = PreProcessor(symbols)
+#     tokens = preproc.process(lexer.tokens, config.input_file)
+
+#     if args.debug == step:
+#         for i in tokens:
+#             print(i)
+#         sys.exit(0)
+
+#     # parser
+#     step += 1
+#     print(f"Phase {step} : Parser")
+#     parser = Parser(tokens)
+#     program = parser.parse_program()
+
+#     if args.debug == step:
+#         program.debug()
+#         sys.exit(0)
+
+
+#     # pseudo instruction expansion
+#     step += 1
+#     pseudo = PseudoInstruction(program)
+#     program = pseudo.process()
+
+#     if args.debug == step:
+#         program.debug()
+#         sys.exit(0)
+
+
+#     # define the assembly sections
+#     sections: Dict[str, Section] = {
+#         ".text": Section(".text"),
+#         ".data": Section(".data"),
+#         ".bss": Section(".bss")
+#     }
+
+#     # semantic analyzer
+#     step += 1
+#     print(f"Phase {step} : Semantic Analyzer - First Pass")
+#     semantic = SemanticAnalyzer(program, symbols, sections)
+#     semantic.first_pass()
+
+#     if args.debug == step:
+#         print("==== SECTIONS ====")
+#         for v in sections.items():
+#             print(v[0])
+#             print(v[1])
+#             print()
+
+#         print("\n==== SYMBOLS ====")
+#         for k, v in symbols.items:
+#             print(v)
+
+#         sys.exit(0)
+
+#     # semantic phase 2
+#     step += 1
+#     print(f"Phase {step} : Semantic Analyzer - Second Pass")
+#     semantic.second_pass()
+
+#     if args.debug == step:
+#         print("==== SECTIONS ====")
+#         for v in sections.items():
+#             print(v[0])
+#             print(v[1])
+#             print()
+
+#         print("\n==== SYMBOLS ====")
+#         for k, v in symbols.items:
+#             print(v)
+
+#         sys.exit(0)
+
+
+# except SyntaxError as e:
+#     print(str(e))
--- compiler/packages/specs.py
+++ compiler/packages/specs.py
@@ -17,70 +17,70 @@ from typing import Any, Dict, List
 
 import re
 
-from packages.token import TokenType
+# from packages.token import TokenType
 
 
 #----- globals
-TOKENS_SPECS = [
-    (TokenType.NUMBER.name, r'0x[a-zA-Z0-9_]+|0b[01_]+|\b\d+\b'),
-    (TokenType.DIRECTIVE.name, r'\.[A-Za-z_][A-Za-z0-9_]*'),
-    (TokenType.LABEL.name, r'[A-Za-z_][A-Za-z0-9_]*:'),
-    (TokenType.IDENT.name, r'[A-Za-z_][A-Za-z0-9_\-\.]*'),
-    (TokenType.ENVVAR.name, r'\$\[[^\]]*\]'),
-    (TokenType.STRING.name, r'\"[^\"]*\"'),
-    (TokenType.CHAR.name, r'\'.\''),
+# TOKENS_SPECS = [
+#     (TokenType.NUMBER.name, r'0x[a-zA-Z0-9_]+|0b[01_]+|\b\d+\b'),
+#     (TokenType.DIRECTIVE.name, r'\.[A-Za-z_][A-Za-z0-9_]*'),
+#     (TokenType.LABEL.name, r'[A-Za-z_][A-Za-z0-9_]*:'),
+#     (TokenType.IDENT.name, r'[A-Za-z_][A-Za-z0-9_\-\.]*'),
+#     (TokenType.ENVVAR.name, r'\$\[[^\]]*\]'),
+#     (TokenType.STRING.name, r'\"[^\"]*\"'),
+#     (TokenType.CHAR.name, r'\'.\''),
 
-    # memory relocation operator
-    (TokenType.M_ABS_LO.name, r'%lo'),
-    (TokenType.M_ABS_HI.name, r'%hi'),
-    (TokenType.M_PCREL_LO.name, r'%pcrel_lo'),
-    (TokenType.M_PCREL_HI.name, r'%pcrel_hi'),
+#     # memory relocation operator
+#     (TokenType.M_ABS_LO.name, r'%lo'),
+#     (TokenType.M_ABS_HI.name, r'%hi'),
+#     (TokenType.M_PCREL_LO.name, r'%pcrel_lo'),
+#     (TokenType.M_PCREL_HI.name, r'%pcrel_hi'),
 
-    # '<' '<<' operators
-    (TokenType.LSHIFT.name, r'<<'),
-    (TokenType.LTE.name, r'<='),
-    (TokenType.LT.name, r'<'),
+#     # '<' '<<' operators
+#     (TokenType.LSHIFT.name, r'<<'),
+#     (TokenType.LTE.name, r'<='),
+#     (TokenType.LT.name, r'<'),
 
-    (TokenType.RSHIFT.name, r'>>'),
-    (TokenType.GTE.name, r'>='),
-    (TokenType.GT.name, r'>'),
+#     (TokenType.RSHIFT.name, r'>>'),
+#     (TokenType.GTE.name, r'>='),
+#     (TokenType.GT.name, r'>'),
 
-    # operators & symbols
-    (TokenType.ASSIGN.name, r'='),
-    (TokenType.LPARENT.name, r'\('),
-    (TokenType.RPARENT.name, r'\)'),
-    (TokenType.COMMA.name, r'\,'),
-    (TokenType.PLUS.name, r'\+'),
-    (TokenType.MINUS.name, r'-'),
-    (TokenType.STAR.name, r'\*'),
-    (TokenType.SLASH.name, r'/'),
-    (TokenType.MODULO.name, r'%'),
-    (TokenType.AND.name, r'&'),
-    (TokenType.OR.name, r'\|'),
-    (TokenType.XOR.name, r'\^'),
-    (TokenType.DOLLAR.name, r'\$'),
+#     # operators & symbols
+#     (TokenType.ASSIGN.name, r'='),
+#     (TokenType.LPARENT.name, r'\('),
+#     (TokenType.RPARENT.name, r'\)'),
+#     (TokenType.COMMA.name, r'\,'),
+#     (TokenType.PLUS.name, r'\+'),
+#     (TokenType.MINUS.name, r'-'),
+#     (TokenType.STAR.name, r'\*'),
+#     (TokenType.SLASH.name, r'/'),
+#     (TokenType.MODULO.name, r'%'),
+#     (TokenType.AND.name, r'&'),
+#     (TokenType.OR.name, r'\|'),
+#     (TokenType.XOR.name, r'\^'),
+#     (TokenType.DOLLAR.name, r'\$'),
 
-    # misc
-    (TokenType.PASTE.name, r'##'),
-    (TokenType.SKIP.name, r'[ \t]+'),
-    (TokenType.COMMENT.name, r'[;#][^\n]*'),
-]
+#     # misc
+#     (TokenType.PASTE.name, r'##'),
+#     (TokenType.SKIP.name, r'[ \t]+'),
+#     (TokenType.COMMENT.name, r'[;#][^\n]*'),
+# ]
 
-# compile the regexp
-ALL_TOKENS = '|'.join(f'(?P<{value}>{pattern})' for value, pattern in TOKENS_SPECS)
-RE_PATTERNS = re.compile(ALL_TOKENS)
+# # compile the regexp
+# ALL_TOKENS = '|'.join(f'(?P<{value}>{pattern})' for value, pattern in TOKENS_SPECS)
+# RE_PATTERNS = re.compile(ALL_TOKENS)
 
 # operators precedence
 # "Please Excuse My Dear Aunt Sally"
-OP_INFO = {
-    '*' : (6, TokenType.STAR),
-    '/' : (6, TokenType.SLASH),
-    '%' : (6, TokenType.MODULO),
-    '+' : (5, TokenType.PLUS),
-    '-' : (5, TokenType.MINUS),
-    '<<': (4, TokenType.LSHIFT),
-    '>>': (4, TokenType.RSHIFT),
-    '&' : (3, TokenType.AND),
-    '^' : (2, TokenType.XOR),
-    '|' : (1, TokenType.OR)
-}
+# OP_INFO = {
+#     '*' : (6, TokenType.STAR),
+#     '/' : (6, TokenType.SLASH),
+#     '%' : (6, TokenType.MODULO),
+#     '+' : (5, TokenType.PLUS),
+#     '-' : (5, TokenType.MINUS),
+#     '<<': (4, TokenType.LSHIFT),
+#     '>>': (4, TokenType.RSHIFT),
+#     '&' : (3, TokenType.AND),
+#     '^' : (2, TokenType.XOR),
+#     '|' : (1, TokenType.OR)
+# }
