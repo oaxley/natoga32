90fbffd47b92df2959f0d3c3fc043fc355ac263e
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-19 16:38:03 -0500

    Add specific patching functions

--- compiler/packages/classes/semantic/reloc_engine.py
+++ compiler/packages/classes/semantic/reloc_engine.py
@@ -59,3 +59,58 @@ class RelocationEngine:
         # retrieve the instruction
         inst = int.from_bytes(self.section.data[offset:offset+4], "big")
 
+    def _hi20(self, value: int) -> int:
+        """Compute HI20 value"""
+        return (value + 0x800) >> 12
+
+    def _lo12(self, value:int, hi20:int) -> int:
+        """Compute LO12 value"""
+        return value - (hi20 << 12)
+
+    def _patch_hi20(self, inst: int, hi20: int) -> int:
+        """Patch Type-U instruction"""
+        inst &= 0xFFF
+        inst |= (hi20 & 0xFFFFF) << 12
+        return inst
+
+    def _patch_lo12_i(self, inst: int, imm12: int) -> int:
+        """Patch Type-I instruction"""
+        inst &= ~(0xFFF << 12)
+        inst |= (imm12 & 0xFFF) << 20
+        return inst
+
+    def _patch_lo12_s(self, inst: int, imm12: int) -> int:
+        """Patch Type-S instruction"""
+        inst &= ~((0x7F << 25) | (0x1F << 7))
+        inst |= ((imm12 >> 5) & 0x7F) << 25     # imm[11:5] -> bits[31:25]
+        inst |= (imm12 & 0x1F) << 7             # imm[4:0] -> bits[11:7]
+        return inst
+
+    def _patch_branch(self, inst: int, delta: int) -> int:
+        """Patch Type-B instruction"""
+        if delta & 0x1:
+            raise SyntaxError("Error: branch target is not aligned")
+
+        imm = delta >> 1
+        inst &= ~((1 << 31) | (0x3F << 25) | (0xF << 8) | (1 << 7))
+
+        inst |= ((imm >> 11) & 0x01) << 31
+        inst |= ((imm >>  5) & 0x3F) << 25
+        inst |= ((imm >>  1) & 0x0F) << 8
+        inst |= ((imm >> 10) & 0x01) << 7
+        return inst
+
+    def _patch_jal(self, inst: int, delta: int) -> int:
+        """Patch Type-J instruction"""
+        if delta & 0x1:
+            raise SyntaxError("Error: jal target is not aligned")
+
+        imm = delta >> 1
+        inst &= ~((1 << 31) | (0x3FF << 21) | (1 << 20) | (0xFF << 12))
+
+        inst |= ((imm >> 19 ) & 0x01 ) << 31
+        inst |= ((imm >>  9 ) & 0x3FF) << 21
+        inst |= ((imm >>  8 ) & 0x01 ) << 20
+        inst |=  (imm & 0xFF) << 12
+
+        return inst
