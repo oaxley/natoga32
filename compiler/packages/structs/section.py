ddd555a010338c6d77900ee43af6083d31fb645f
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-19 17:34:06 -0500

    Rename offset to size

--- compiler/packages/structs/section.py
+++ compiler/packages/structs/section.py
@@ -27,14 +27,18 @@ class Section:
     - name (str): the name of the section
     - data (bytearray): the data to store in this section
     - relocations (list): the relocation list
-    - offset (int): the current write cursor
+    - offset (int): total size of the section
     - address (int): section start address
     """
     name: str
     data: bytearray = field(default_factory=bytearray)
     relocations: List[Relocation] = field(default_factory=list)
-    offset: int = 0
+    size: int = 0
     address: int = 0
 
     def __repr__(self) -> str:
-        return f"""data: {self.data}\noffset: {self.offset}\nrelocations: {self.relocations}"""
+        return f"""data: {self.data}
+size: {self.size}
+address: 0x{self.address:08X}
+relocations: {self.relocations}
+"""
