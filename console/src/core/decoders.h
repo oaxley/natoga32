6f72c8b049bb70302921ff9b2505c5dfd5814825
slegrand <ut5svsxgc@relay.firefox.com>
2025-12-30 16:53:39 -0500

    Add Risc-V decoder functions + test cases

---
+++ console/src/core/decoders.h
@@ -0,0 +1,74 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/license/mit
+ *
+ * @author	Sebastien LEGRAND
+ * @license	MIT License
+ *
+ * @brief	RISC-V decoder functions
+ */
+#pragma once
+
+// program-specific headers
+#include "vc/types.h"
+
+namespace vc::decoder {
+
+u32 opcode(u32 instr) {
+    return instr & 0x7f;
+}
+
+u32 rd(u32 instr) {
+    return (instr >> 7) & 0x1F;
+}
+
+u32 rs1(u32 instr) {
+    return (instr >> 15) & 0x1F;
+}
+
+u32 rs2(u32 instr) {
+    return (instr >> 20) & 0x1F;
+}
+
+u32 funct3(u32 instr) {
+    return (instr >> 12) & 0x7;
+}
+
+u32 funct7(u32 instr) {
+    return (instr >> 25) & 0x7F;
+}
+
+i32 i_immediate(u32 instr) {
+    return static_cast<i32>(instr & 0xFFF0'0000) >> 20;
+}
+
+i32 s_immediate(u32 instr) {
+    u32 imm = ((instr >> 7) & 0x1F) | ((instr >> 25) << 5);
+    return static_cast<i32>(imm << 20) >> 20;     // sign extended
+}
+
+i32 b_immediate(u32 instr) {
+    u32 imm = ((instr >>  8) & 0x0F) <<  1 |
+              ((instr >> 25) & 0x3F) <<  5 |
+              ((instr >>  7) & 0x01) << 11 |
+              ((instr >> 31) & 0x01) << 12;
+    return static_cast<i32>(imm << 19) >> 19;     // sign extended
+}
+
+i32 u_immediate(u32 instr) {
+    return static_cast<i32>(instr & 0xFFFF'F000);    // no shift, value is imm[32:12]
+}
+
+i32 j_immediate(u32 instr) {
+    u32 imm = ((instr >> 21) & 0x3FF) <<  1 |
+              ((instr >> 20) &   0x1) << 11 |
+              ((instr >> 12) &  0xFF) << 12 |
+              ((instr >> 31) &   0x1) << 20;
+    return static_cast<i32>(imm << 11) >> 11;     // sign extendee
+}
+
+} // namespace vc::decoder
---
+++ tests/libvc/test_decoder.cpp
@@ -0,0 +1,175 @@
+#include <catch2/catch_all.hpp>
+
+#include "core/decoders.h"
+
+using namespace vc;
+
+TEST_CASE("Instruction decoder", "[cpu][decode]") {
+
+    SECTION("Decode opcode") {
+        // ADDI x1, x0, 42
+        u32 instruction = 0x02A00093;
+        REQUIRE(decoder::opcode(instruction) == 0x13);
+    }
+
+    SECTION("Decode rd") {
+        // ADDI x2, x1, 10
+        u32 instruction = 0x00A08113;
+        REQUIRE(decoder::rd(instruction) == 2);
+    }
+
+    SECTION("Decode rs1") {
+        // ADDI x2, x1, 10
+        u32 instruction = 0x00A08113;
+        REQUIRE(decoder::rs1(instruction) == 1);
+    }
+
+    SECTION("Decode rs2") {
+        // ADDI x3, x1, x2
+        u32 instruction = 0x002081B3;
+        REQUIRE(decoder::rs2(instruction) == 2);
+    }
+
+    SECTION("Decode funct3") {
+        // AND x6, x4, x5
+        u32 instruction = 0x00527333;
+        REQUIRE(decoder::funct3(instruction) == 7);
+    }
+
+    SECTION("Decode funct7") {
+        // SUB x1, x2, x3
+        u32 instruction = 0x402081B3;
+        REQUIRE(decoder::funct7(instruction) == 0b00100000);
+    }
+}
+
+TEST_CASE("S-Type immedidate decoding", "[cpu][decode]") {
+
+    SECTION("Positive immediate: 20") {
+        // SW x5, 20(x10)
+        u32 instruction = 0x00552A23;
+        REQUIRE(decoder::s_immediate(instruction) == 20);
+    }
+
+    SECTION("Negative immediate: -8") {
+        // SW x12, -8(x8)
+        u32 instruction = 0xFEC42C23;
+        REQUIRE(decoder::s_immediate(instruction) == -8);
+    }
+
+    SECTION("Maximum positive immediate: 2047") {
+        u32 instruction = 0x7E000FA3;
+        REQUIRE(decoder::s_immediate(instruction) == 2047);
+    }
+
+    SECTION("Maximum negative immediate: -2048") {
+        u32 instruction = 0x80000023;
+        REQUIRE(decoder::s_immediate(instruction) == -2048);
+    }
+}
+
+TEST_CASE("I-Type immedidate decoding", "[cpu][decode]") {
+
+    SECTION("Positive immediate: 42") {
+        // ADDI x5, x10, 42
+        u32 instruction = 0x02A50293;
+        REQUIRE(decoder::i_immediate(instruction) == 42);
+    }
+
+    SECTION("Negative immediate: -5") {
+        // ADDI x1, x2, -5
+        u32 instruction = 0xFFB10093;
+        REQUIRE(decoder::i_immediate(instruction) == -5);
+    }
+
+    SECTION("Maximum positive immediate: 2047") {
+        u32 instruction = 0x7FF00093;
+        REQUIRE(decoder::i_immediate(instruction) == 2047);
+    }
+
+    SECTION("Maximum negative immediate: -2048") {
+        u32 instruction = 0x80000093;
+        REQUIRE(decoder::i_immediate(instruction) == -2048);
+    }
+}
+
+TEST_CASE("B-Type immedidate decoding", "[cpu][decode]") {
+
+    SECTION("Positive immediate: 100") {
+        // BEQ x1, x2, 100
+        u32 instruction = 0x06208263;
+        REQUIRE(decoder::b_immediate(instruction) == 100);
+    }
+
+    SECTION("Negative immediate: -8") {
+        // BNE x5, x6, -8
+        u32 instruction = 0xFE629CE3;
+        REQUIRE(decoder::b_immediate(instruction) == -8);
+    }
+
+    SECTION("Maximum positive immediate: 4094") {
+        // BEQ x0, x0, 4094
+        u32 instruction = 0x7E000FE3;
+        REQUIRE(decoder::b_immediate(instruction) == 4094);
+    }
+
+    SECTION("Maximum negative immediate: -4096") {
+        // BEQ x0, x0, -4096
+        u32 instruction = 0x80000063;
+        REQUIRE(decoder::b_immediate(instruction) == -4096);
+    }
+}
+
+TEST_CASE("U-Type immedidate decoding", "[cpu][decode]") {
+
+    SECTION("Positive immediate: 0x12345000") {
+        // LUI x5, 0x12345
+        u32 instruction = 0x123452B7;
+        REQUIRE(decoder::u_immediate(instruction) == (i32)0x12345000);
+    }
+
+    SECTION("Negative immediate: 0xFFFFF000") {
+        // LUI x6, 0xFFFFF
+        u32 instruction = 0xFFFFF337;
+        REQUIRE(decoder::u_immediate(instruction) == (i32)0xFFFFF000);
+    }
+
+    SECTION("Maximum positive immediate: 0x7FFFF000") {
+        // LUI x7, 0x7FFFF
+        u32 instruction = 0x7FFFF3B7;
+        REQUIRE(decoder::u_immediate(instruction) == (i32)0x7FFFF000);
+    }
+
+    SECTION("Maximum negative immediate: 0x80000000") {
+        // LUI x8, 0x80000
+        u32 instruction = 0x80000437;
+        REQUIRE(decoder::u_immediate(instruction) == (i32)0x80000000);
+    }
+}
+
+TEST_CASE("J-Type immedidate decoding", "[cpu][decode]") {
+
+    SECTION("Positive immediate: 1024") {
+        // JAL x1, 1024
+        u32 instruction = 0x400000EF;
+        REQUIRE(decoder::j_immediate(instruction) == 1024);
+    }
+
+    SECTION("Negative immediate: -512") {
+        // JAL x1, -512
+        u32 instruction = 0xE01FF0EF;
+        REQUIRE(decoder::j_immediate(instruction) == -512);
+    }
+
+    SECTION("Maximum positive immediate: +1048574") {
+        // JAL x1, 1048574
+        u32 instruction = 0x7FFFF0EF;
+        REQUIRE(decoder::j_immediate(instruction) == 1048574);
+    }
+
+    SECTION("Maximum negative immediate: -1048576") {
+        // JAL x1, -1048576
+        u32 instruction = 0x800000EF;
+        REQUIRE(decoder::j_immediate(instruction) == -1048576);
+    }
+}
