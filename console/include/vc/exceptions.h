c0f6ee3cf7c28442905e521024247dc67ec936a5
slegrand <ut5svsxgc@relay.firefox.com>
2026-01-01 17:36:49 -0500

    Add CPU exceptions values

---
+++ console/include/vc/exceptions.h
@@ -0,0 +1,30 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/license/mit
+ *
+ * @author	Sebastien LEGRAND
+ * @license	MIT License
+ *
+ * @brief	Virtual Console - Exceptions values
+ */
+
+#pragma once
+
+// program-specific includes
+#include "vc/types.h"
+
+
+namespace vc
+{
+
+// CPU Exceptions
+constexpr u32 CPU_THREAD_MAIN_EXIT_ERROR            = 0x00010101;
+constexpr u32 CPU_THREAD_SPAWN_ERROR                = 0x00010102;
+constexpr u32 CPU_THREAD_STACK_OVERFLOW_ERROR       = 0x00010103;
+constexpr u32 CPU_THREAD_DEADLOCK                   = 0x00010104;
+
+} // namespace vc
