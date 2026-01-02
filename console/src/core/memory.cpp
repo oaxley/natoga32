88545fe75b8d4eb86ee88fddf1ebfd5be796131c
slegrand <ut5svsxgc@relay.firefox.com>
2026-01-02 17:48:58 -0500

    Cosmetic changes

--- console/src/core/memory.cpp
+++ console/src/core/memory.cpp
@@ -23,33 +23,39 @@
 namespace vc {
 
 Memory::Memory() :
-    ram_(MMAP_RAM_SIZE) {
+    ram_(MMAP_RAM_SIZE)
+{
     reset();
 }
 
-void Memory::reset() {
+void Memory::reset()
+{
     std::memset(ram_.data(), 0, ram_.size());
 }
 
-u8 Memory::read8(u32 addr) const {
+u8 Memory::read8(u32 addr) const
+{
     if (addr < ram_.size()) {
         return ram_[addr];
     }
     return 0;
 }
 
-u16 Memory::read16(u32 addr) const {
+u16 Memory::read16(u32 addr) const
+{
     return read8(addr) | (read8(addr + 1) << 8);
 }
 
-u32 Memory::read32(u32 addr) const {
+u32 Memory::read32(u32 addr) const
+{
     return read8(addr) |
            (read8(addr + 1) << 8) |
            (read8(addr + 2) << 16) |
            (read8(addr + 3) << 24);
 }
 
-void Memory::write8(u32 addr, u8 value) {
+void Memory::write8(u32 addr, u8 value)
+{
     // ensure ROM remains RO
     if (addr < MMAP_ROM_BOOT_LOADER + MMAP_ROM_SIZE - 1) {
         return;
@@ -57,12 +63,14 @@ void Memory::write8(u32 addr, u8 value) {
     ram_[addr] = value;
 }
 
-void Memory::write16(u32 addr, u16 value) {
+void Memory::write16(u32 addr, u16 value)
+{
     write8(addr, value & 0xFF);
     write8(addr + 1, (value >> 8) & 0xFF);
 }
 
-void Memory::write32(u32 addr, u32 value) {
+void Memory::write32(u32 addr, u32 value)
+{
     write8(addr, value & 0xFF);
     write8(addr+1, (value >> 8) & 0xFF);
     write8(addr+2, (value >> 16) & 0xFF);
