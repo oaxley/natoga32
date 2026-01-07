5101becd5b445c6d0e281ef9bbbb3093978de4ad
slegrand <ut5svsxgc@relay.firefox.com>
2026-01-06 22:32:50 -0500

    Rewrite InstructionTest class to facilitate Thread testing

--- tests/libvc/test_cpu/instruction_test.h
+++ tests/libvc/test_cpu/instruction_test.h
@@ -1,39 +1,68 @@
 // Helper class for instruction testing
 #pragma once
+#include <array>
 
 #include "core/memory.h"
 #include "core/cpu.h"
 
 using namespace vc;
 
+struct ThreadTest
+{
+    u32 size;           // code size
+    u32 addr;           // current code address
+    u32 old_pc;         // previous PC before execution
+};
+
 class InstructionTest
 {
+private:
+    std::array<ThreadTest, THREADS_COUNT> thread_;
+
 public:
     Memory mem;
     CPU cpu;
-    int tid;
-    u32 old_pc = 0;
-    u32 new_pc = 0;
 
     InstructionTest() :
-        mem(), cpu(mem), tid{0} {
-    }
-
-    ThreadContext& getCtx(int tid = 0) {
-        return cpu.getThreadContext(tid);
-    }
-
-    void loadInstruction(u32 instr, u32 offset = 0, int tid = 0) {
-        u32 addr = MMAP_CODE + offset;
-        mem.write32(addr, instr);
-        if (old_pc == 0) {
-            getCtx(tid).pc = old_pc = addr;
+        thread_{}, mem(), cpu(mem)
+    {
+        for (int tid = 0; tid < THREADS_COUNT; tid++) {
+            thread_[tid].size = 0;
+            thread_[tid].addr = MMAP_CODE;
+            thread_[tid].old_pc = 0;
         }
     }
 
-    void execute() {
+    ThreadContext& getCtx(int tid = 0)
+    {
+        return cpu.getThreadContext(tid);
+    }
+
+    void loadInstruction(u32 instr, u32 offset = 0, int tid = 0)
+    {
+        u32 addr = thread_[tid].addr + offset + (thread_[tid].size * sizeof(u32));
+        mem.write32(addr, instr);
+        if (thread_[tid].size == 0) {
+            getCtx(tid).pc = addr;
+            thread_[tid].old_pc = addr;
+        }
+        thread_[tid].size++;
+    }
+
+    void reset(int tid = 0)
+    {
+        thread_[tid].size = 0;
+        thread_[tid].addr = MMAP_CODE;
+        thread_[tid].old_pc = MMAP_CODE;
+    }
+
+    u32 getPC(int tid = 0) {
+        return getCtx(tid).pc;
+    }
+
+    void execute()
+    {
         cpu.tick();
-        new_pc = getPC();
     }
 
     u32 getReg(int r, int tid = 0) {
@@ -44,11 +73,10 @@ public:
         getCtx(tid).registers[r] = value;
     }
 
-    u32 getPC(int tid = 0) {
-        return getCtx(tid).pc;
-    }
-
-    u32 deltaPC() {
-        return new_pc - old_pc;
+    u32 deltaPC(int tid = 0) {
+        u32 new_pc = getCtx(tid).pc;
+        u32 delta = new_pc - thread_[tid].old_pc;
+        thread_[tid].old_pc = new_pc;
+        return delta;
     }
 };
