90368ae626b019dffb3b3bb33594c308ad862aaf
slegrand <ut5svsxgc@relay.firefox.com>
2026-01-15 22:27:02 -0500

    goasm: main entrypoint

---
+++ tools/goasm/cmd/goasm/main.go
@@ -0,0 +1,9 @@
+// goasm - NATOGA32 RISC-V Assembler
+//
+// This source file is subject to the MIT License
+// that is bundled with this package in the file LICENSE.txt.
+package main
+
+func main() {
+	Execute()
+}
