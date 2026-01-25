be094acfd08e386211c727f00b522270905d73b9
slegrand <ut5svsxgc@relay.firefox.com>
2026-01-25 17:05:22 -0500

    Rework messages structures

--- console/include/vc/debug.h
+++ console/include/vc/debug.h
@@ -23,27 +23,36 @@ namespace vc::debug
 enum class MessageType : u16
 {
     // Requests (debugger -> host)
-    GetState = 0x0001,
-    ReadMemory = 0x0002,
-    WriteMemory = 0x0003,
-    SetBreakpoint = 0x0004,
-    ClearBreakpoint = 0x0005,
-    StepInstruction = 0x0006,
-    Continue = 0x0007,
-    Pause = 0x0008,
-    GetVRAM = 0x0009,
-    GetThreads = 0x000A,
+    GetConsoleState     = 0x0001,
+    SetConsoleState     = 0x0002,
+    ReadMemory          = 0x0003,
+    WriteMemory         = 0x0004,
+    ReadCSR             = 0x0005,
+    WriteCSR            = 0x0006,
+    GetThreadInfo       = 0x0007,
+    SetBreakpoint       = 0x0008,
+    ClearBreakpoint     = 0x0009,
+    StepInstruction     = 0x000A,
+    GetVRAM             = 0x000B,
 
-    // Responses (host -> debugger)
-    StateUpdate = 0x1001,
-    MemoryData = 0x1002,
-    BreakpointHit = 0x1003,
-    ThreadsInfo = 0x1004,
-    VRAMData = 0x1005,
+    // Response (host -> debugger)
+    ConsoleStateUpdate  = 0x1001,
+    MemoryData          = 0x1002,
+    CSRData             = 0x1003,
+    ThreadInfo          = 0x1004,
+    BreakpointHit       = 0x1005,
+
+    // observer pattern subscription
+    SubConsoleState     = 0x2001,
+    SubMemory           = 0x2002,
+    SubCSR              = 0x2003,
+    SubThread           = 0x2004,
+    SubVRAM             = 0x2005,
 
     // Events
-    Reset = 0x2001,
-    Paused = 0x2002,
+    Reset               = 0x3001,
+    Pause               = 0x3002,
+    Resume              = 0x3003
 };
 
 // remove padding in structures as we want to stay the most efficient
@@ -54,38 +63,90 @@ struct MessageHeader {
     u32 sequence;   // Sequence number for matching requets/response
 };
 
-struct CPUState {
+//----- ConsoleState (0x0001 / 0x0002)
+// Get: request
+// MessageHeader with type = 0x0001, size = 0
+
+// Get: response
+struct GetConsoleStateRep {
+    u64 cycles;
     u32 regs[32];
     u32 pc;
-    u64 cycles;
+    u32 thread_states;
     u8 current_thread;
-    u8 thread_states;
+    u8 state;
 };
 
-struct MemoryRequest {
+// Set: request
+struct SetConsoleStateReq {
+    u8 state;
+};
+
+//----- Memory (0x0003 / 0x0004)
+// Read: request
+struct MemoryReadReq {
     u32 address;
     u32 size;
 };
 
-struct MemoryData {
+// Read: response
+struct MemoryReadRep {
     u32 address;
     u32 size;
-    // followed by actual data
+    // actual data
 };
 
-struct Breakpoint {
+// Write: request
+struct MemoryWriteReq {
     u32 address;
-    u8 thread_id;       // 0xFF for all threads
-    u8 enabled;
+    u32 size;
+    // data
 };
 
-struct ThreadInfo {
+//----- CSR (0x0005 / 0x0006)
+// Read: request
+struct CSRReadReq {
+    u16 reg;
+};
+
+// Read: response
+struct CSRReadRep {
+    u16 reg;
+    u32 value;
+};
+
+// Write: request
+struct CSRWriteReq {
+    u16 reg;
+    u32 value;
+};
+
+//----- ThreadInfo (0x0007)
+// ThreadInfo: request
+struct GetThreadInfoReq {
+    u8 id;
+};
+
+// ThreadInfo: response
+struct GetThreadInfoRep {
     u8 id;
     u8 state;
     u32 pc;
-    u64 sleep_until;
     u32 regs[32];
+    u32 waitkey;
+    u64 sleep_until;
+    u64 cycles;
 };
+
+//----- Breakpoint (0x0008/0x0009/0x000A)
+// set
+struct BreakpointReq {
+    u8 thread_id;
+    u8 enabled;
+    u32 address;
+};
+
+
 #pragma pack(pop)
 
 } // namespace vc::debug
