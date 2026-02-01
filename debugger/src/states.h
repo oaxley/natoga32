b00bad101adb144e2937948d41cb4ed63d7103c5
slegrand <ut5svsxgc@relay.firefox.com>
2026-02-01 18:22:43 -0500

    Add State variables structure

---
+++ debugger/src/states.h
@@ -0,0 +1,20 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/license/mit
+ *
+ * @author	Sebastien LEGRAND
+ * @license	MIT License
+ *
+ * @brief	Debugger states values
+ */
+#pragma once
+
+struct States
+{
+    // show the Help > About window
+    bool show_about = false;
+};
