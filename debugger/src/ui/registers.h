aaaf3e97d6cf93259ab041d663c5be8cbe84688f
slegrand <ut5svsxgc@relay.firefox.com>
2026-02-02 22:30:38 -0500

    Basic widget to show registers content

---
+++ debugger/src/ui/registers.h
@@ -0,0 +1,61 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/license/mit
+ *
+ * @author	Sebastien LEGRAND
+ * @license	MIT License
+ *
+ * @brief	Basic widgets to show the registers
+ */
+#pragma once
+
+// standard library headers
+#include <array>
+#include <imgui.h>
+
+// program-specific includes
+#include "generic.h"
+
+// class definition
+class Registers : public IGeneric
+{
+public:
+    Registers(DebugClient& client, GLFWwindow* window, const std::array<uint32_t, 32>& regs) :
+        IGeneric(client, window), regs_{regs}
+    { }
+
+    void render()
+    {
+        if (ImGui::BeginTable("Registers", 4, ImGuiTableFlags_SizingFixedFit)) {
+            ImGui::TableSetupColumn("##name1", ImGuiTableColumnFlags_WidthFixed, 30.0f);
+            ImGui::TableSetupColumn("##val1", ImGuiTableColumnFlags_WidthFixed, 70.0f);
+            ImGui::TableSetupColumn("##name2", ImGuiTableColumnFlags_WidthFixed, 30.0f);
+            ImGui::TableSetupColumn("##val2", ImGuiTableColumnFlags_WidthFixed, 70.0f);
+
+            for (int y = 0; y < 16; y++) {
+                ImGui::TableNextColumn();
+                ImGui::Text("x%d", y);
+                ImGui::TableNextColumn();
+                ImGui::Text("%08X", regs_[y]);
+
+                ImGui::TableNextColumn();
+                ImGui::Text("x%d", y+16);
+                ImGui::TableNextColumn();
+                ImGui::Text("%08X", regs_[y+16]);
+
+                if (y < 15)
+                    ImGui::TableNextRow();
+            }
+
+            ImGui::EndTable();
+        }
+    }
+
+private:
+    std::array<uint32_t, 32> regs_;
+
+};
