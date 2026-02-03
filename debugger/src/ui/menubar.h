89af8161eeef5e95fdae09b1977ca5c17564bda0
slegrand <ut5svsxgc@relay.firefox.com>
2026-02-02 21:05:10 -0500

    Wire all the menus with their corresponding state

--- debugger/src/ui/menubar.h
+++ debugger/src/ui/menubar.h
@@ -29,10 +29,12 @@ void showMenubar(States& state)
         // File Menu
         if (ImGui::BeginMenu("File")) {
             if (ImGui::MenuItem("Load Symbols")) {
+                state = States::LoadSymbols;
             }
 
             ImGui::Separator();
             if (ImGui::MenuItem("Preferences")) {
+                state = States::Preferences;
             }
 
             ImGui::Separator();
@@ -45,18 +47,24 @@ void showMenubar(States& state)
         // Edit Menu
         if (ImGui::BeginMenu("Edit")) {
             if (ImGui::MenuItem("Undo", "Ctrl+Z")) {
+                state = States::Undo;
             }
             if (ImGui::MenuItem("Redo", "Ctrl+Y")) {
+                state = States::Redo;
             }
             ImGui::Separator();
             if (ImGui::MenuItem("Copy", "Ctrl+C")) {
+                state = States::Copy;
             }
             if (ImGui::MenuItem("Paste", "Ctrl+V")) {
+                state = States::Paste;
             }
             ImGui::Separator();
             if (ImGui::MenuItem("Find", "Ctrl+F")) {
+                state = States::Find;
             }
             if (ImGui::MenuItem("Replace", "Ctrl+H")) {
+                state = States::Replace;
             }
             ImGui::EndMenu();
         }
@@ -67,15 +75,20 @@ void showMenubar(States& state)
             // memory
             if (ImGui::BeginMenu("Memory")) {
                 if (ImGui::MenuItem("Heap")) {
+                    state = States::Heap;
                 }
                 if (ImGui::MenuItem(".BSS")) {
+                    state = States::Bss;
                 }
                 if (ImGui::MenuItem(".DATA")) {
+                    state = States::Data;
                 }
                 if (ImGui::MenuItem(".TEXT")) {
+                    state = States::Text;
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Explorer")) {
+                    state = States::Explorer;
                 }
                 ImGui::EndMenu();
             }
@@ -83,19 +96,24 @@ void showMenubar(States& state)
             // Code
             if (ImGui::BeginMenu("Code")) {
                 if (ImGui::MenuItem("Source")) {
+                    state = States::Source;
                 }
                 ImGui::Separator();
                 if (ImGui::BeginMenu("Threads")) {
                     if (ImGui::MenuItem("Info")) {
+                        state = States::ThreadsInfo;
                     }
                     if (ImGui::MenuItem("Stack")) {
+                        state = States::ThreadsStack;
                     }
                     if (ImGui::MenuItem("Local Storage")) {
+                        state = States::ThreadsStorage;
                     }
                     ImGui::EndMenu();
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Breakpoints")) {
+                    state = States::Breakpoints;
                 }
                 ImGui::EndMenu();
             }
@@ -103,15 +121,20 @@ void showMenubar(States& state)
             // CSR & Signals
             if (ImGui::BeginMenu("CSR/Signals")) {
                 if (ImGui::MenuItem("Control Status Reg.")) {
+                    state = States::CSR;
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Clock")) {
+                    state = States::Clock;
                 }
                 if (ImGui::MenuItem("Video VSYNC")) {
+                    state = States::VSync;
                 }
                 if (ImGui::MenuItem("Video HSYNC")) {
+                    state = States::HSync;
                 }
                 if (ImGui::MenuItem("Interrupts")) {
+                    state = States::Interrupts;
                 }
                 ImGui::EndMenu();
             }
@@ -120,45 +143,58 @@ void showMenubar(States& state)
             if (ImGui::BeginMenu("Video")) {
                 if (ImGui::BeginMenu("Buffer")) {
                     if (ImGui::MenuItem("FrameBuffer A")) {
+                        state = States::FrameBufferA;
                     }
                     if (ImGui::MenuItem("FrameBuffer B")) {
+                        state = States::FrameBufferB;
                     }
                     if (ImGui::MenuItem("Z-Buffer")) {
+                        state = States::ZBuffer;
                     }
                     if (ImGui::MenuItem("Text Layer")) {
+                        state = States::TextLayer;
                     }
                     ImGui::EndMenu();
                 }
                 ImGui::Separator();
                 if (ImGui::BeginMenu("Sprites")) {
                     if (ImGui::MenuItem("Viewer")) {
+                        state = States::SpriteViewer;
                     }
                     if (ImGui::MenuItem("Attributes")) {
+                        state = States::SpriteAttributes;
                     }
                     if (ImGui::MenuItem("Palettes")) {
+                        state = States::SpritePalettes;
                     }
                     ImGui::EndMenu();
                 }
                 ImGui::Separator();
                 if (ImGui::BeginMenu("BG")) {
                     if (ImGui::MenuItem("Viewer")) {
+                        state = States::BGViewer;
                     }
                     if (ImGui::MenuItem("Map")) {
+                        state = States::BGMap;
                     }
                     if (ImGui::MenuItem("Palettes")) {
+                        state = States::BGPalettes;
                     }
                     ImGui::EndMenu();
                 }
                 ImGui::Separator();
                 if (ImGui::BeginMenu("Fonts")) {
                     if (ImGui::MenuItem("Viewer")) {
+                        state = States::FontViewer;
                     }
                     if (ImGui::MenuItem("Palettes")) {
+                        state = States::FontPalettes;
                     }
                     ImGui::EndMenu();
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Affine Matrices")) {
+                    state = States::Affine;
                 }
                 ImGui::EndMenu();
             }
@@ -170,17 +206,18 @@ void showMenubar(States& state)
         if (ImGui::BeginMenu("Console")) {
             if (ImGui::BeginMenu("Run")) {
                 if (ImGui::MenuItem("Step")) {
-
+                    state = States::RunStep;
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Pause")) {
-
+                    state = States::RunPause;
                 }
                 if (ImGui::MenuItem("Resume")) {
+                    state = States::RunResume;
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Reset")) {
-
+                    state = States::RunReset;
                 }
                 ImGui::EndMenu();
             }
@@ -195,9 +232,10 @@ void showMenubar(States& state)
         // Tools
         if (ImGui::BeginMenu("Tools")) {
             if (ImGui::MenuItem("Hex Converter")) {
+                state = States::HexConverter;
             }
-
             if (ImGui::MenuItem("Instr. Encoder")) {
+                state = States::InstrEncoder;
             }
 
             ImGui::EndMenu();
