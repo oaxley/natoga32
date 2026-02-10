15230f9ddaa20a7dc8e0b7bc53a03595ee9311f5
slegrand <ut5svsxgc@relay.firefox.com>
2026-02-09 21:49:01 -0500

    Add windows constant IDs

--- debugger/src/constants.h
+++ debugger/src/constants.h
@@ -15,9 +15,17 @@
 
 // standard library headers
 #include <string>
+#include <stdint.h>
 
 // program-specific includes
 
 
 // semantic versionning
 constexpr std::string VERSION = "0.1.0";
+
+
+// windows ID
+constexpr uint8_t ABOUT_HWND = 0x01;
+
+
+constexpr uint8_t QUIT_HWND = 0xFF;         //< specific value for Quit
