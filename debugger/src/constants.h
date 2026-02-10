1f85830349809e71736d2427f220e28aebcfcbc2
slegrand <ut5svsxgc@relay.firefox.com>
2026-02-09 23:34:07 -0500

    Add ThreadInfo window ID

--- debugger/src/constants.h
+++ debugger/src/constants.h
@@ -23,9 +23,16 @@
 // semantic versionning
 constexpr std::string VERSION = "0.1.0";
 
+// registers + threads
+constexpr uint8_t RegisterCount = 32;
+constexpr uint8_t ThreadCount = 8;
+
 
 // windows ID
+// maximum number of windows in the debugger
+constexpr uint8_t WindowCount = 10;
+
+constexpr uint8_t QUIT_HWND = 0x00;
+
 constexpr uint8_t ABOUT_HWND = 0x01;
-
-
-constexpr uint8_t QUIT_HWND = 0xFF;         //< specific value for Quit
+constexpr uint8_t THREADINFO_HWND = 0x02;
